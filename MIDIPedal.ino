44c61189d08159a28be308d8b49769fb45da5d1d
Marco <27402195+mksoc@users.noreply.github.com>
2018-07-14 14:25:37 +0200

    Initial file

---
+++ MIDIPedal.ino
@@ -0,0 +1,79 @@
+#include "Button.h" //include debounce and correct press reading
+
+#define SIZEOF_ARRAY(arr) (sizeof(arr) / sizeof(arr[0])) //to compute sizeof or array passed to function
+
+//Commands declarations
+const byte patchChange = 0xc0;
+const byte tunerOn[] = {0xb0, 0x4a, 64}; //command sequence to turn on tuner
+const byte tunerOff[] = {0xb0, 0x4a, 63}; //command sequence to turn off tuner
+const byte idReq[] = {0xf0, 0x7e, 0x00, 0x06, 0x01, 0xf7}; //command sequence to request ID
+const byte testCommand[] = {0xf0,0x52,0x00,0x5f,0x29,0xf7};
+
+//Tuner variables
+const int tunerPin = 2; //pin for tuner button, active high
+Button tunerButton = Button(tunerPin, false, false, 25);
+bool tunerActive = false; //tuner status
+
+void setup() 
+{
+  //initialize serial @ MIDI baudrate 
+  //WARNING: serial does not work if you pass a variable to begin() --> must be a number!!
+  Serial.begin(115200); 
+}
+
+void loop() 
+{
+  tunerButton.read();
+  if (tunerButton.wasPressed())
+  {
+    checkID();
+    sendMIDI(testCommand, SIZEOF_ARRAY(testCommand));
+  }
+}
+
+void sendMIDI(byte *command, size_t len) 
+{
+  for (int i = 0; i < len; i++)
+  {
+    Serial.write(command[i]);
+  }
+}
+
+void sendMIDI(byte command, byte data)
+{
+  Serial.write(command);
+  Serial.write(data);
+}
+
+void sendMIDI(byte command, byte data1, byte data2)
+{
+  Serial.write(command);
+  Serial.write(data1);
+  Serial.write(data2);
+}
+
+void tunerToggle()
+{
+  if (!tunerActive)
+    {
+      sendMIDI(tunerOn, SIZEOF_ARRAY(tunerOn));
+      tunerActive = true;
+    }
+    else
+    {
+      sendMIDI(tunerOff, SIZEOF_ARRAY(tunerOff));
+      tunerActive = false;
+    }
+}
+
+void checkID()
+{
+  sendMIDI(idReq, SIZEOF_ARRAY(idReq)); //request ID
+  //may read answer and check if ID is correct
+}
+
+void patchSelect(int patchNumber)
+{
+  byte num = (byte)(patchNumber-1);
+  sendMIDI(patchChange, num);
+}
